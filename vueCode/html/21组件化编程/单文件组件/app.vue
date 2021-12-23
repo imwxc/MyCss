22f20581299d31a020a24f94e0229207f375f74f
imwxc <1362451456@qq.com>
2021-12-23 16:44:57 +0800

    update

---
+++ vueCode/html/21组件化编程/单文件组件/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <MySchool></MySchool>
+      <MyStudent></MyStudent>
+  </div>
+</template>
+
+<script>
+import MySchool from "./MySchool.vue"
+import MyStudent from "./MyStudent.vue"
+export default {
+    name: "App",
+    components:{
+        MyStudent,
+        MySchool
+    }
+}
+</script>
+
+<style>
+
+</style>
