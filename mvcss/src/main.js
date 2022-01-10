ddd8b1139ef63a491e4664b9a5a73a1916a4a151
imwxc <1362451456@qq.com>
2022-01-10 15:18:57 +0800

    commit

--- mvcss/src/main.js
+++ mvcss/src/main.js
@@ -1,8 +1,12 @@
+// 整个项目的入口文件
+
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 创建Vue实例对象
 new Vue({
+  // 将App组件放入容器中
   render: h => h(App),
 }).$mount('#app')
