ad9625f1216109f1399c6ae32e68ed6adb68989b
imwxc <1362451456@qq.com>
2022-01-27 11:34:08 +0800

    update

--- mvcss/src/App.vue
+++ mvcss/src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <MySchool></MySchool>
+      <MyStudent></MyStudent>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MySchool from "./MySchool.vue"
+import MyStudent from "./MyStudent.vue"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: "App",
+    components:{
+        MyStudent,
+        MySchool
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ mvcss/src/components/MyStudent.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- 组件的结构 -->
+    <div class="demo">
+        <h2>MyStudentname:{{ MyStudentname }}</h2>
+        <h2>MyStudentaddress:{{ MyStudentaddress }}</h2>
+    </div>
+</template>
+
+<script>
+// 组件交互的代码
+    export default {
+        name:"MyStudent",
+        data(params) {
+            return { 
+            MyStudentname: "MyStudent123",
+            MyStudentaddress: "MyStudentAddress123",
+            };
+        },
+        methods: {},
+    };
+</script> 
+
+<style>
+</style>
+ 
