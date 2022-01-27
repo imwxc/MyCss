787d56e9e129114538a65c8820e656016db679da
imwxc <1362451456@qq.com>
2022-01-27 15:39:44 +0800

    update

--- mvcss/src/App.vue
+++ mvcss/src/App.vue
@@ -2,17 +2,16 @@
   <div>
       <img src="./assets/logo.png" alt="logo">
       <MySchool></MySchool>
-      <MyStudent></MyStudent>
+      <MySchool></MySchool>
+      <MySchool></MySchool>
   </div>
 </template>
 
 <script>
 import MySchool from "./MySchool.vue"
-import MyStudent from "./MyStudent.vue"
 export default {
     name: "App",
     components:{
-        MyStudent,
         MySchool
     }
 }
