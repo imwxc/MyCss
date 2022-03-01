0bf68209f6cc6d8afc58795504c59fcdac0e3a91
imwxc <1362451456@qq.com>
2022-03-01 14:10:32 +0800

    update

--- mvcss/src/App.vue
+++ mvcss/src/App.vue
@@ -1,20 +1,12 @@
 <template>
   <div>
-      <MySchool></MySchool>
-      <MyStudent />
-      <MyStudent />
-      <MyStudent />
   </div>
 </template>
 
 <script>
-import MyStudent from './components/MyStudent';
-import MySchool from './components/MySchool'
 export default {
     name: "App",
     components:{
-        MyStudent,
-        MySchool
     },
     methods:{
     }
