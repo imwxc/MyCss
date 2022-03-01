6b3d0f0da4f24c5489557f7652fc3c82169914a9
imwxc <1362451456@qq.com>
2022-03-01 16:23:03 +0800

    update

--- mvcss/src/components/list.vue
+++ mvcss/src/components/list.vue
@@ -1,17 +1,44 @@
 <template>
 	<div>
-        <Task>
-    </div>
+		<ul class="todo-main">
+			<li>
+				<label>
+					<input type="checkbox" />
+					<span>yyyy</span>
+				</label>
+				<button class="btn btn-danger" style="display: none">
+					删除
+				</button>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-import Task from "./Task"
+import Task from "./Task";
 export default {
 	name: "list",
-    components:{
-        Task
-    }
+	components: {
+		Task,
+	},
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+/*main*/
+.todo-main {
+	margin-left: 0px;
+	border: 1px solid #ddd;
+	border-radius: 2px;
+	padding: 0px;
+}
+
+.todo-empty {
+	height: 40px;
+	line-height: 40px;
+	border: 1px solid #ddd;
+	border-radius: 2px;
+	padding-left: 5px;
+	margin-top: 10px;
+}
+</style>
