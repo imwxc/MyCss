02651197d50708fa2b5875a11fda83b47c811b1d
imwxc <1362451456@qq.com>
2022-03-07 19:30:54 +0800

    update

---
+++ mvcss/src_ToDoList基础版/components/Bottom.vue
@@ -0,0 +1,77 @@
+<template>
+	<div class="todo-footer">
+		<label>
+			<input type="checkbox" v-model="isAll" />
+		</label>
+		<span>
+			<span>已完成{{ finishTakskNum }}</span> / 全部{{ taskList.length }}
+		</span>
+		<button class="btn btn-danger" @click="clearTasks">
+			清除已完成任务
+		</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "bottom",
+	props: {
+		taskList: {
+			type: Array,
+		},
+		emptyTaskList: {
+			type: Function,
+		},
+	},
+	computed: {
+		finishTakskNum() {
+			return this.taskList.filter((item) => item.complated === true)
+				.length;
+		},
+		isAll: {
+			get() {
+				return this.finishTakskNum === this.taskList.length;
+			},
+			set(newValue) {
+				this.checkAll(newValue);
+			},
+		},
+	},
+	methods: {
+		clearTasks() {
+			this.emptyTaskList();
+		},
+		checkAll(newValue) {
+			this.taskList.forEach((item) => (item.complated = newValue));
+		},
+	},
+};
+</script>
+
+<style scoped>
+/*footer*/
+.todo-footer {
+	height: 40px;
+	line-height: 40px;
+	padding-left: 6px;
+	margin-top: 5px;
+}
+
+.todo-footer label {
+	display: inline-block;
+	margin-right: 20px;
+	cursor: pointer;
+}
+
+.todo-footer label input {
+	position: relative;
+	top: -1px;
+	vertical-align: middle;
+	margin-right: 5px;
+}
+
+.todo-footer button {
+	float: right;
+	margin-top: 5px;
+}
+</style>
---
+++ mvcss/src_ToDoList基础版/components/Task.vue
@@ -0,0 +1,82 @@
+<template>
+	<div>
+		<li>
+			<label>
+				<input type="checkbox" v-model="item.complated" />
+				<span>{{ item.title }}</span>
+			</label>
+			<button class="btn btn-danger" @click="handleDelete(item.id)">删除</button>
+		</li>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Task",
+	props: {
+		item: {
+			type: Object,
+			value: {},
+		},
+		index: {
+			type: Number,
+			value: -1,
+		},
+        removeTask:{
+            type:Function
+        }
+	},
+    methods: {
+        handleDelete(id){
+            if(confirm("确定删除？")){
+                // 通知App删除对象
+                this.removeTask(id);
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+li {
+	list-style: none;
+	height: 36px;
+	line-height: 36px;
+	padding: 0 5px;
+	border-bottom: 1px solid #ddd;
+}
+
+li label {
+	float: left;
+	cursor: pointer;
+}
+
+li label li input {
+	vertical-align: middle;
+	margin-right: 6px;
+	position: relative;
+	top: -1px;
+}
+
+li button {
+	float: right;
+	visibility: hidden;
+	margin-top: 3px;
+}
+
+li:before {
+	content: initial;
+}
+
+li:last-child {
+	border-bottom: none;
+}
+
+li:hover {
+	background-color: #ddd;
+}
+
+li:hover button {
+	visibility:visible;
+}
+</style>
