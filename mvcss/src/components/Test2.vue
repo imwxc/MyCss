9d55439860346f6be866413971b6abf43920afeb
imwxc <1362451456@qq.com>
2022-09-29 17:37:43 +0800

    update

---
+++ mvcss/src/components/Test2.vue
@@ -0,0 +1,56 @@
+<template>
+	<div>
+		test2
+		<button @click="click">显示/隐藏</button>
+		<transition name="hello" :appear="true">
+			<h1 v-show="show">nihaoa!</h1>
+		</transition>
+		<!-- 多个元素的过度 -->
+		<transition-group name="hello" :appear="true">
+			<h1 v-show="show" key="1">nihaoa!11</h1>
+			<h1 v-show="show" key="2">nihaoa!22</h1>
+		</transition-group>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Test2",
+	data() {
+		return {
+			show: true
+		};
+	},
+	methods: {
+		click() {
+			this.show = !this.show;
+		}
+	}
+};
+</script>
+
+<style scoped>
+/*base*/
+body {
+	background: #fff;
+}
+h1 {
+	background-color: orange;
+}
+
+.hello-enter-active,
+.hello-leave-active {
+	transition: 0.5s ease-in-out;
+}
+
+/* 离开起点 */
+.hello-leave,
+.hello-enter-to {
+	transform: translateX(0px);
+}
+/* 进入起点 */
+.hello-enter,
+.hello-leave-to {
+	transform: translateX(-100%);
+}
+</style>
---
+++ mvcss/src_vue过度动画/components/Test3.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		test2
+		<button @click="click">显示/隐藏</button>
+		<transition name="animate__animated animate__bounce" :appear="true" enter-active-class="animate__swing" leave-active-class="animate__zoomOut">
+			<h1 v-show="show">nihaoa!</h1>
+		</transition>
+		<!-- 多个元素的过度 -->
+		<!-- <transition-group name="hello" :appear="true">
+			<h1 v-show="show" key="1">nihaoa!11</h1>
+			<h1 v-show="show" key="2">nihaoa!22</h1>
+		</transition-group> -->
+	</div>
+</template>
+
+<script>
+import "animate.css";
+export default {
+	name: "Test3",
+	data() {
+		return {
+			show: true
+		};
+	},
+	methods: {
+		click() {
+			this.show = !this.show;
+		}
+	}
+};
+</script>
+
+<style scoped>
+/*base*/
+body {
+	background: #fff;
+}
+h1 {
+	background-color: orange;
+}
+</style>
