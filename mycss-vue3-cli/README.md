32a95bf603564ef5f47f1595e618e03437555d03
imwxc <1362451456@qq.com>
2023-04-15 15:09:55 +0800

    update

--- mycss-vue3-cli/README.md
+++ mycss-vue3-cli/README.md
@@ -1,24 +1,15 @@
 # mycss-vue3-cli
 
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+### setup
+    1.Vue3的新配置项，值为一个函数
+    2.setup是所有组合api的地方
+    3.组件中用的数据方法都在setup中配置
+    4.seup返回值：
+        1.返回一个对象则 对象中的属性方法均可在模板中使用
+        2.返回一个渲染函数，可以自定义渲染内容
+    5.注意点：
+        1.尽量不和vue2配置混用
+        2.setup中不能访问vue2的配置
+        3.重名时vue3优先
+        4.setup不能为async函数
+    
--- mycss-vue3-cli/src/App.vue
+++ mycss-vue3-cli/src/App.vue
@@ -1,30 +1,51 @@
 <template>
-  <nav>
+	<!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view />
+  <router-view /> -->
+	<div>测试setup写法{{ name }}{{ age }}</div>
 </template>
 
 <style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 
 nav {
-  padding: 30px;
+	padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+	a {
+		font-weight: bold;
+		color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+		&.router-link-exact-active {
+			color: #42b983;
+		}
+	}
 }
 </style>
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import h from "vue";
+export default {
+	name: "App",
+	setup(props) {
+		let name = "name";
+		let age = 12;
+		function hello(params) {
+			alert(`'哦我叫'+${name}+${age}`);
+		}
+		// return ()=>h('h1','header')
+		return {
+			name,
+			age,
+			hello,
+		};
+	},
+};
+</script>
