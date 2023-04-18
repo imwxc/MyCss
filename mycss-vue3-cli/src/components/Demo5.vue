594cc956333b5e9b4e53b131349aaf3a4bcc7be5
imwxc <1362451456@qq.com>
2023-04-19 00:38:31 +0800

    update

--- mycss-vue3-cli/src/components/Demo5.vue
+++ mycss-vue3-cli/src/components/Demo5.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<!-- <div>测试setup写法{{ name }}{{ age }}</div> -->
-        <h2>当前鼠标点击坐标为： x{{ x }} ， y{{ y }}</h2>
+		<h2>当前鼠标点击坐标为： x{{ x }} ， y{{ y }}</h2>
 	</div>
 </template>
 
@@ -18,17 +18,17 @@
 import { ref, reactive, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from 'vue';
 export default {
 	name: "Demo5",
-    emits: ['hello'],
+	emits: ['hello'],
 	setup() {
-        // context.emit('hello')
+		// context.emit('hello')
 		// let name = ref("name"); // reference 
 		// let age = ref(12);
-        let x = ref(999)
-        let y = ref(0)
+		let x = ref(999)
+		let y = ref(0)
 		return {
 			// name,
 			// age
-            x,y
+			x, y
 		};
 	},
 };
