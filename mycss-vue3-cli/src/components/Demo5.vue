06671f86888cdcd9e33a76019208dc666978fda7
imwxc <1362451456@qq.com>
2023-04-19 01:26:25 +0800

    update

--- mycss-vue3-cli/src/components/Demo5.vue
+++ mycss-vue3-cli/src/components/Demo5.vue
@@ -1,45 +1,46 @@
 <template>
-  <div>
-    <!-- <div>测试setup写法{{ name }}{{ age }}</div> -->
-    <h2>当前鼠标点击坐标为： x{{ x }} ， y{{ y }}</h2>
-  </div>
+    <div>
+        <!-- <div>测试setup写法{{ name }}{{ age }}</div> -->
+        <h2>当前鼠标点击坐标为： x{{ x }} ， y{{ y }}</h2>
+    </div>
 </template>
-<script>
+<script lang="ts">
 import {
-  ref,
-  reactive,
-  onBeforeMount,
-  onMounted,
-  onBeforeUpdate,
-  onUpdated,
-  onBeforeUnmount,
-  onUnmounted,
+    ref,
+    reactive,
+    onBeforeMount,
+    onMounted,
+    onBeforeUpdate,
+    onUpdated,
+    onBeforeUnmount,
+    onUnmounted,
 } from "vue";
 export default {
-  name: "DemoFive",
-  emits: ["hello"],
-  setup() {
-    // context.emit('hello')
-    // let name = ref("name"); // reference
-    // let age = ref(12);
-    let x = ref(999);
-    let y = ref(0);
-    return {
-      // name,
-      // age
-      x,
-      y,
-    };
-  },
+    name: "DemoFive",
+    emits: ["hello"],
+    setup() {
+        // context.emit('hello')
+        // let name = ref("name"); // reference
+        // let age = ref(12);
+        let ponit = reactive({
+            x: 999,
+            y: 0,
+        });
+        return {
+            // name,
+            // age
+            ponit,
+        };
+    },
 };
 </script>
 
 <style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
